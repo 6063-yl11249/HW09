f9d56d2870c233be72478db16b1a1eef8be5573e
Yuanqing <yl11249@nyu.edu>
2023-11-07 18:47:26 -0500

    jump1.0

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@ let platforms = [];
 let gravity = 0.5;
 let jumpForce = -10;
 let score = 0;
+let left = false;
+let right = false;
+let platformBuffer = 150; // 控制平台生成的最小间隔
 
 function setup() {
   createCanvas(400, 600);
@@ -10,9 +13,13 @@ function setup() {
   // Initialize player as a square sprite
   player = createSprite(width / 2, height - 50, 50, 50);
 
-  // Create initial platforms
-  for (let i = 0; i < 5; i++) {
-    let plat = createSprite(random(50, width - 50), i * 120, 100, 20);
+  // Create an initial platform for the player to stand on
+  let initialPlat = createSprite(width / 2, height - 25, 100, 20);
+  platforms.push(initialPlat);
+
+  // Generate some platforms off the screen to start
+  for (let i = 1; i < 5; i++) {
+    let plat = createSprite(random(50, width - 50), height - i * platformBuffer, 100, 20);
     platforms.push(plat);
   }
 }
@@ -24,9 +31,9 @@ function draw() {
   player.velocity.y += gravity;
 
   // Player movement
-  if (keyIsDown(LEFT_ARROW)) {
+  if (left) {
     player.velocity.x = -5;
-  } else if (keyIsDown(RIGHT_ARROW)) {
+  } else if (right) {
     player.velocity.x = 5;
   } else {
     player.velocity.x = 0;
@@ -40,30 +47,35 @@ function draw() {
     }
   }
 
-  // Move platforms down and regenerate new ones
+  // When the player is above a third of the screen, move platforms down
   if (player.position.y < height / 3) {
     player.position.y = height / 3;
-
-    // Move platforms down
-    for (let i = platforms.length - 1; i >= 0; i--) {
-      platforms[i].position.y += abs(player.velocity.y);
-      if (platforms[i].position.y > height) {
-        platforms[i].remove();
-        platforms.splice(i, 1);
-      }
+    for (let plat of platforms) {
+      plat.position.y += abs(player.velocity.y);
     }
 
-    // Add new platform at the top
-    let newPlat = createSprite(random(50, width - 50), player.position.y - height, 100, 20);
-    platforms.push(newPlat);
-
-    // Increase score
+    // Increase score as player goes up
     score++;
+  }
+
+  // Remove off-screen platforms and add new ones
+  for (let i = platforms.length - 1; i >= 0; i--) {
+    if (platforms[i].position.y > height) {
+      platforms[i].remove();
+      platforms.splice(i, 1);
+    }
+  }
+
+  // If there are less than 5 platforms, add a new one
+  if (platforms.length < 5) {
+    let newPlatY = platforms[platforms.length - 1].position.y - platformBuffer;
+    let newPlat = createSprite(random(50, width - 50), newPlatY, 100, 20);
+    platforms.push(newPlat);
   }
 
   // Check for game over
   if (player.position.y > height) {
-    noLoop(); // Stop the draw loop
+    noLoop();
     console.log("Game Over! Score: " + score);
   }
 
@@ -77,3 +89,18 @@ function draw() {
   text(score, width / 2, 50);
 }
 
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    left = true;
+  } else if (keyCode === RIGHT_ARROW) {
+    right = true;
+  }
+}
+
+function keyReleased() {
+  if (keyCode === LEFT_ARROW) {
+    left = false;
+  } else if (keyCode === RIGHT_ARROW) {
+    right = false;
+  }
+}
